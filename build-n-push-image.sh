f30cb8f3f4b92bed43ef1876c8a51751acb97b7c
Xin Zhang <zxaws@amazon.com>
2022-07-10 08:58:52 +0000

    add k8s menifest & update docker build script

---
+++ build-n-push-image.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+arg_count=$#
+script_name=$(basename $0)
+script_action=""
+
+if test $arg_count -eq 1; then
+  if [[ $1 =~ ^(ecr|dockerhub)$ ]]; then
+    script_action=$1
+  else
+    echo "Script Action must be ecr or dockerhub"
+    echo "Usage: $script_name [ecr|dockerhub]"
+    exit -1
+  fi
+else
+  echo "Usage: $script_name [ecr|dockerhub]"
+  echo ""
+  echo "Examples:"
+  echo "$script_name ecr"
+  echo ""
+  exit 0
+fi
+
+# Get script location.
+SHELL_PATH=$(cd "$(dirname "$0")";pwd)
+
+pushToEcr()
+{
+    local aws_account_id="$(aws sts get-caller-identity --output text --query 'Account')"
+    local deployment_region="$(aws configure get region)"
+
+    # app's image repository.
+    local aspdotnet_repo="dotnetasp-sigterm-demo"
+    # app's image repository URI.
+    local aspdotnet_repo_uri=$aws_account_id.dkr.ecr.$deployment_region.amazonaws.com/$aspdotnet_repo
+
+    aws ecr describe-repositories --repository-names $aspdotnet_repo --region $deployment_region &> /dev/null
+    local result=$?
+    if [ $result -ne 0 ]; then
+        echo "Create image repository..."
+        aws ecr create-repository --repository-name $aspdotnet_repo --region $deployment_region
+    fi
+
+    echo "Login to ECR..."
+    local DOCKER_LOGIN_CMD=$(aws ecr get-login --no-include-email --region $deployment_region)
+    eval "${DOCKER_LOGIN_CMD}"
+
+    echo "Build docker image..."
+    local image_tag="$(echo $(date '+%Y.%m.%d.%H%M%S' -d '+8 hours'))"
+    docker buildx build --push --platform linux/arm64,linux/amd64 --tag $aspdotnet_repo_uri:latest --tag $aspdotnet_repo_uri:$image_tag ${SHELL_PATH}/.
+
+    echo
+    echo "Done"
+}
+
+pushToDockerHub()
+{
+    local aspdotnet_repo_uri=cowcoa/dotnetasp-sigterm-demo
+
+    echo "Login to DockerHub..."
+    docker login registry-1.docker.io
+
+    echo "Build docker image..."
+    docker buildx build --push --platform linux/arm64,linux/amd64 --tag $aspdotnet_repo_uri:latest ${SHELL_PATH}/.
+
+    echo
+    echo "Done"
+}
+
+if [ $script_action = ecr ]; then
+    pushToEcr
+else
+    pushToDockerHub
+fi
